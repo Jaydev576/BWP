580d7ea58c00dc79335ff3b923e25edd6221b558
Harshil <theharshiile@gmail.com>
2024-10-16 17:23:14 +0530

    Done

--- public/pages/templates/previewTheme.js
+++ public/pages/templates/previewTheme.js
@@ -1,6 +1,9 @@
-let cards = document.querySelectorAll('.card')
+let cards = document.querySelectorAll('.previewBtn')
 
-// cards.forEach(item => {
-//     item.addEventListener('click', async () => {
-//     })
-// })
+cards.forEach(item => {
+    item.addEventListener('click', async () => {
+        setTimeout(() => {
+            // window.location.href = '/preview/theme1'
+        }, 1200);
+    })
+})
